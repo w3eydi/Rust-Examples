68fb3459e1cc74b03b0455eff267d8abb5e8aaa3
w3eydi <w3.eydi@gmail.com>
2021-11-13 23:14:51 +0300

    Added form and required dependencies.
     - Changed Cargo.toml file.
     - Added Rocket.toml file for 'templates'.
     - main.rs is displaying the simple form structure.
     - Added 'static' folder with 'FileServer'.
    	It can contain files like css formatter.
     - Added index.html.tera and macros.html.tera.

--- rocket-db/src/main.rs
+++ rocket-db/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use rocket::fs::{FileServer, relative};
+use rocket::form::{Context};
+use rocket_dyn_templates::{Template, context};
+
+#[get("/")]
+fn index() -> Template {
+    /*Template::render("index", context!{
+        title: "Hello"
+    })*/
+    Template::render("index", &Context::default())
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+        .mount("/", FileServer::from(relative!("static")))
 }
