6b7b1d9e04d64eea94c50197e809f987db81d7a6
w3eydi <w3.eydi@gmail.com>
2021-11-14 12:27:24 +0300

    Added success page and integration is complete.
     - success.html.tera
     - Added two struct types.

--- rocket-db/src/main.rs
+++ rocket-db/src/main.rs
@@ -1,21 +1,46 @@
 #[macro_use] extern crate rocket;
 
-use rocket::fs::{FileServer, relative};
-use rocket::form::{Context};
-use rocket_dyn_templates::{Template, context};
+use rocket::fs::{FileServer, TempFile, relative};
+use rocket::http::{Status, ContentType};
+use rocket::form::{Form, Context, Contextual, FromForm};
+
+use rocket_dyn_templates::{Template};
+
+#[derive(Debug, FromForm)]
+struct Submission<'v> {
+    #[field(validate = len(1..))]
+    title: &'v str,
+    #[field(validate = len(1..250))]
+    r#abstract: &'v str
+}
+
+#[derive(Debug, FromForm)]
+struct Submit<'s> {
+    submission: Submission<'s>
+}
 
 #[get("/")]
 fn index() -> Template {
-    /*Template::render("index", context!{
-        title: "Hello"
-    })*/
     Template::render("index", &Context::default())
+}
+
+#[post("/", data="<form>")]
+fn submit<'a>(form: Form<Contextual<'a, Submit<'a>>>) -> (Status, Template) {
+    let template = match form.value {
+        Some(ref submission) => {
+            //println!("{:#?}", title);
+            Template::render("success", &form.context)
+        }
+        None => Template::render("index", &form.context)
+    };
+
+    (form.context.status(), template)
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index])
+        .mount("/", routes![index, submit])
         .attach(Template::fairing())
         .mount("/", FileServer::from(relative!("static")))
 }
