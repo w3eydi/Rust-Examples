d84bb0f0c9a5e2270a8210a5db4df33ef8622cd1
w3eydi <w3.eydi@gmail.com>
2022-05-09 20:32:31 +0300

    Added basic blockchain template.

---
+++ blockchain/src/main.rs
@@ -0,0 +1,12 @@
+use blockchainlib::*;
+
+fn main() {
+    let mut block = Block::new(0, 0, vec![0; 32], 0, "eydi".to_owned());
+    println!("{:?}", &block);
+
+    let h = block.hash();
+    println!("{:?}", &h);
+
+    block.hash = h;
+    println!("{:?}", &block);
+}
