509535487c1d4327433ea870dcf583f59407530c
w3eydi <w3.eydi@gmail.com>
2023-01-19 21:14:25 +0300

    Added actix_examples.
     - Added first example. 'static serving'

---
+++ actix_examples/Readme.md
@@ -0,0 +1,1 @@
+[Static Code Example](serving_static/src/main.rs)
---
+++ actix_examples/serving_static/src/main.rs
@@ -0,0 +1,22 @@
+use actix_web::{web, App, HttpRequest, HttpServer, Result};
+use actix_files::{NamedFile, Files};
+
+async fn index() -> Result<NamedFile> {
+    Ok(NamedFile::open("./static/index.html")?)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(
+                Files::new("/static", "static")
+                    .show_files_listing(),
+            )
+            .route("/", web::get().to(index))
+
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
