3d2d757b9a53a0d57e02ec90caa542c95e034597
w3eydi <w3.eydi@gmail.com>
2023-02-03 22:39:29 +0300

    Added custom headers.
     - Updated Readme.

--- axum_examples/Readme.md
+++ axum_examples/Readme.md
@@ -13,3 +13,4 @@ By examining the `routing` section, you can see the preview components of a basi
 - `localhost:3000/path_variables/91 or number` -> Path variables example of axum.
 - `localhost:3000/query_params?message=hello&id=23` -> Query params.
 - `localhost:3000/mirror_user_agent` -> User agent example.
+- `localhost:3000/mirror_custom_header` -> Custom header example.
--- axum_examples/routing/src/routes/mod.rs
+++ axum_examples/routing/src/routes/mod.rs
@@ -4,11 +4,13 @@ mod mirror_body_json;
 mod path_variables;
 mod query_params;
 mod mirror_user_agent;
+mod mirror_custom_header;
 
 use axum::{Router, routing::{patch, post, get} };
 use mirror_body_string::mirror_body_string;
 use mirror_body_json::mirror_body_json;
 use query_params::query_params;
+use mirror_custom_header::mirror_custom_header;
 
 pub fn creating_routes() -> Router {
     Router::new()
@@ -19,4 +21,5 @@ pub fn creating_routes() -> Router {
         .route("/path_variables/91", get(path_variables::hard_coded_path))
         .route("/query_params", get(query_params))
         .route("/mirror_user_agent", get(mirror_user_agent::mirror_user_agent))
+        .route("/mirror_custom_header", get(mirror_custom_header))
 }
